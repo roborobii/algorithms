aac418627290ac439199e0625a34657f684aee53
zepl_robin <robin@MacBook-Pro.zepl.local>
2020-02-11 15:27:03 -0800

    more efficient than concatination

--- reversewords_icio_001.go
+++ reversewords_icio_001.go
@@ -1,10 +1,16 @@
 package main
 import (
 	"fmt"
+	"bytes"
+	"strings"
 )
 func main() {
 	const sentence string = "i live and code"
 	result := reverseWords(sentence)
+	fmt.Println(result)
+	result = reverseWords_BytesBuffer(sentence)
+	fmt.Println(result)
+	result = reverseWords_StringBuilder(sentence)
 	fmt.Println(result)
 }
 
@@ -12,7 +18,6 @@ func reverseWords(sentence string) string {
 	// O(N) time (linear scan through sentence) 
 	// O(N) space (stored result)
 	// N being the number of characters in the sentence string
-
 	var result string
 	end := len(sentence)
 	for i := end - 1; i >= 0; i-- {
@@ -22,4 +27,38 @@ func reverseWords(sentence string) string {
 		}
 	}
 	return result + sentence[:end]
+}
+
+func reverseWords_BytesBuffer(sentence string) string {
+	// More efficient way to concantinate strings than reverseWords
+	// instead of `result +=` which creates strings everytime
+	// we instead should use bytes buffer
+	var result bytes.Buffer
+	end := len(sentence)
+	for i := end - 1; i >= 0; i-- {
+		if string(sentence[i]) == " " {
+			result.WriteString(sentence[i+1:end])
+			result.WriteString(" ")
+			end = i
+		}
+	}
+	result.WriteString(sentence[:end])
+	return result.String()
+}
+
+func reverseWords_StringBuilder(sentence string) string {
+	// Also a more efficient way to concantinate strings than reverseWords
+	// instead of `result +=` which creates strings everytime
+	// we instead should use strings.Builder (for Go 1.10+)
+	var result strings.Builder
+	end := len(sentence)
+	for i := end - 1; i >= 0; i-- {
+		if string(sentence[i]) == " " {
+			result.WriteString(sentence[i+1:end])
+			result.WriteString(" ")
+			end = i
+		}
+	}
+	result.WriteString(sentence[:end])
+	return result.String()
 }
